593729a8104e987547748adaf9443a0dd2bd41fd
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-08-08 02:41:01 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome to the Sample App!'
+      flash[:success] = t('flash.success.user_create')
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
