d21aa621a26df61fe04b23dca1b2f8338235616d
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-08-11 12:40:20 +0900

    finish 11.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,14 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # メインのサンプルユーザーを1人作成する
 # メインのサンプルユーザーを1人作成する
+# メインのサンプルユーザーを1人作成する
 User.create!(name: 'Example User',
              email: 'example@railstutorial.org',
              password: 'foobar',
              password_confirmation: 'foobar',
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
@@ -23,5 +26,7 @@ User.create!(name: 'Example User',
   User.create!(name:,
                email:,
                password:,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
