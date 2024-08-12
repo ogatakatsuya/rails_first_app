927c1fc03c782a86b9866fcbf0da110e2bbc8262
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-08-12 14:49:57 +0900

    fix

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,8 +7,10 @@ class Micropost < ApplicationRecord
   end
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
-                                      message: t('msg.error.file_extention_violation') },
-                      size: { less_than: 5.megabytes,
-                              message: t('msg.error.file_size_violation') }
+  error_size = 'should be less than 5MB'
+  error_ext = 'must be a valid image format'
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+                                    message: error_ext },
+                    size: { less_than: 5.megabytes,
+                            message: error_size }
 end
